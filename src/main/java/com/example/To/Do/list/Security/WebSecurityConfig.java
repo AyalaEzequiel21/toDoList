4e329f0e4618dcd91872c3ef76bbf47cb9e0a4ed
Checho <ezequielayala.c@gmail.com>
2023-07-03 21:25:14 -0300

    configuration web security

--- src/main/java/com/example/To/Do/list/Security/WebSecurityConfig.java
+++ src/main/java/com/example/To/Do/list/Security/WebSecurityConfig.java
@@ -1,7 +1,25 @@
 package com.example.To.Do.list.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity, AuthenticationManager authManager) throws Exception {
+        httpSecurity
+                .csrf((csrf) ->
+                        csrf.disable())
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/signup").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+                return httpSecurity.build();
+
+    }
 }
