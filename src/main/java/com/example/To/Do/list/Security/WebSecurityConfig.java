47378edc20f69c5f8ff3b4d3a09724bdf6768b27
Checho <ezequielayala.c@gmail.com>
2023-07-04 22:16:23 -0300

    update configuration spring security

--- src/main/java/com/example/To/Do/list/Security/WebSecurityConfig.java
+++ src/main/java/com/example/To/Do/list/Security/WebSecurityConfig.java
@@ -3,7 +3,17 @@ package com.example.To.Do.list.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,13 +23,49 @@ public class WebSecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity httpSecurity, AuthenticationManager authManager) throws Exception {
         httpSecurity
                 .csrf((csrf) ->
-                        csrf.disable())
+                        csrf.disable()
+                )
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/signup").permitAll()
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
+
+                )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement((man) -> man
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
                 return httpSecurity.build();
+    }
 
+    @Bean
+    UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles()
+                .build()
+        );
+        return manager;
+    }
+
+    @Bean
+    DaoAuthenticationProvider authProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public ProviderManager authManagerBean(HttpSecurity security) throws Exception {
+        return (ProviderManager) security.getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(authProvider()).
+                build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
